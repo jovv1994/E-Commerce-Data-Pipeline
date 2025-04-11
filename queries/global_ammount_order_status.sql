ae6e000d5cf36d543ebc861ad5bef3cddb4c309a
John Vasconez <jovv1994@outlook.com>
2025-04-11 10:27:02 -0500

    John Vasconez - Script global_ammount_order_status.sql

--- queries/global_ammount_order_status.sql
+++ queries/global_ammount_order_status.sql
@@ -1,3 +1,7 @@
 -- TODO: This query will return a table with two columns; order_status, and
 -- Ammount. The first one will have the different order status classes and the
 -- second one the total ammount of each.
+SELECT oo.order_status,
+COUNT(*) AS Ammount
+FROM olist_orders oo
+GROUP BY oo.order_status;
